b97805be2f5f8defc7b0725f0913b054eb606d6e
Ryan C. Thompson <rct@thompsonclan.org>
2011-01-01 14:09:18 -0500

    Added script boilerplate

---
+++ mkplaylist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import plac
+    @plac.annotations(
+        # (helptext, kind, abbrev, type, choices, metavar)
+        )
+    def main():
+        pass
